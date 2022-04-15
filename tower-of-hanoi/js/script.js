e8d5ca7b7e752994f0a4809a41ca45b7ccf57217
Alexandre Uglar <acuglar@gmail.com>
2022-04-15 04:26:35 -0300

    Render disks

--- tower-of-hanoi/js/script.js
+++ tower-of-hanoi/js/script.js
@@ -7,12 +7,11 @@ form.addEventListener('submit', e => {
   e.preventDefault()
   selectedDisks = e.target.disksNumber.value
 
-  for (let i = 0; i < selectedDisks; i++) {
+  for (let i = 1; i <= selectedDisks; i++) {
     const disk = document.createElement('div')
-    disk.style.background = 'linear-gradient(to right, #d7b889, #b27315, #966f33)'
-    disk.style.width = `${i * 10}%`
-    disk.style.height = '40px';
-    disk.style.zIndex = '999'
+    disk.style.background = `linear-gradient(to right, ${colors[i][0]}, ${colors[i][1]}, ${colors[i][2]})`
+    disk.style.width = `${i / selectedDisks * 100}%`
+    disk.style.height = '10%';
     column.append(disk)
   }
 })
@@ -22,9 +21,9 @@ const colors = {
   1: ['#c595f1', '#8A2BE2', '#6820aa'],
   2: ['#ef9280', '#DF2500', '#a71c00'],
   3: ['#ffae60', '#FF7E00', '#bf5f00'],
-  4: ['#fde6f3', '#FBCCE7', '#dcb3ca'],
-  5: ['#f3f380', '#E6E600', '#adad00'],
-  6: ['#e4fff2', '#C9FFE5', '#97bfac'],
-  7: ['#c0ffd5', '#70df95', '#509f6a'],
-  8: ['#80dbf5', '#00b7eb', '#007293'],
+  4: ['#ffd760', '#FFBF00', '#bf8f00'],
+  5: ['#ffdf80', '#E6E600', '#adad00'],
+  6: ['#c0ffd5', '#70df95', '#509f6a'],
+  7: ['#c0f5ff', '#80EAFF', '#70cddf'],
+  8: ['#e4fff2', '#C9FFE5', '#97bfac'],
 }
