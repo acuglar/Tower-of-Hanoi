aec10a33c4abdf911b4b2c9ca20a5388c99323e5
Alexandre Uglar <acuglar@gmail.com>
2022-05-02 20:45:17 -0300

    Toggle game select

--- index.js
+++ index.js
@@ -4,13 +4,18 @@ const gameTitle = document.querySelector('main > h1')
 const game = document.querySelector('iframe')
 const expandIcon = document.querySelector('i')
 
-gameSelected.addEventListener('click', e => {
+gameSelected.addEventListener('click', async e => {
   if (e.target.matches('li')) {
+    // const gameAnchor = e.target.parentElement
+    const gameTarget = e.target.getAttribute('target')
+
     gameTitle.textContent = e.target.textContent
+    game.setAttribute('src', `./${gameTarget}/index.html`)
+    game.setAttribute('name', `${gameTarget}`)
   }
-  console.log(e.target, e.target.matches('a'))
 })
 
-expandIcon.addEventListener('click', e => {
+expandIcon.addEventListener('click', () => {
   game.requestFullscreen()
 })
+
