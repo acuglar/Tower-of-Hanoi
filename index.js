06294936476ad840c57d586f32be5bc217cc8b72
Alexandre Uglar <acuglar@gmail.com>
2022-05-02 23:53:09 -0300

    Style game title typography

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const gameContainer = document.querySelector('main')
 const gameSelected = document.querySelector('nav > ul')
-const gameTitle = document.querySelector('main > h1')
+const gameTitle = document.querySelector('[data-font]')
 const game = document.querySelector('iframe')
 const expandIcon = document.querySelector('i')
 
 gameSelected.addEventListener('click', async e => {
   if (e.target.matches('li')) {
-    // const gameAnchor = e.target.parentElement
     const gameTarget = e.target.getAttribute('target')
 
     gameTitle.textContent = e.target.textContent
+    gameTitle.setAttribute('data-font', `${gameTarget}`)
     game.setAttribute('src', `./${gameTarget}/index.html`)
     game.setAttribute('name', `${gameTarget}`)
   }
